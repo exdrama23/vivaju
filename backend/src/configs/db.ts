3bc24db771f00dc4f07f3c28143b37bedc862419
exdrama23 <alecviniciusflamengo@gmail.com>
2026-05-20 10:10:30 -0300

    Correção de build

--- backend/src/configs/db.ts
+++ backend/src/configs/db.ts
@@ -1,17 +1,9 @@
 import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('A variável de ambiente DATABASE_URL não foi definida.');
-}
-
-const pool = new Pool({ 
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL.includes('localhost') ? false : { rejectUnauthorized: false }
+const prisma = new PrismaClient({
+  // @ts-ignore - Prisma 7 exige um campo de conexão no construtor se não houver no schema,
+  // mas usará a variável de ambiente DATABASE_URL automaticamente se o campo for omitido no schema.
+  datasourceUrl: process.env.DATABASE_URL
 });
-
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
 
 export default prisma;
