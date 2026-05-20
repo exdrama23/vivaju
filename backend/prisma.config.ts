d5572a13514017fca8c76e6632aca6b5312b1108
exdrama23 <alecviniciusflamengo@gmail.com>
2026-05-20 10:36:52 -0300

    agora vai

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  engineType: "library",
   migrations: {
     path: "prisma/migrations",
   },
--- backend/src/configs/db.ts
+++ backend/src/configs/db.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from '../generated/prisma/client';
 
 const prisma = new PrismaClient({
-  // @ts-ignore - Prisma 7 exige um campo de conexão no construtor se não houver no schema,
-  // mas usará a variável de ambiente DATABASE_URL automaticamente se o campo for omitido no schema.
-  datasourceUrl: process.env.DATABASE_URL
-});
+    log: ['error', 'warn']
+} as any);
 
 export default prisma;
